7426691eb75ae6b3f6f3bb1eae7536551a9dc4e0
Ubeath <soloschmail@gmail.com>
2024-09-22 20:26:23 +0100

    add toaster

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,12 @@ import { HeroHighlight } from "./ui/Highlight";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import Header from "./ui/Header";
 import Footer from "./ui/Footer";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
-
   return (
     <DndProvider backend={HTML5Backend}>
       <HeroHighlight className="min-h-dvh w-full grid grid-rows-[auto_2fr_auto]  ">
-      
         <Header />
         <main className="">
           <section className=" mx-auto overflow-auto p-10 max-w-3xl">
@@ -19,6 +18,27 @@ const App = () => {
         </main>
         <Footer />
       </HeroHighlight>
+
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            background: "#4d5ec2",
+            color: "white",
+          },
+        }}
+      />
     </DndProvider>
   );
 };
